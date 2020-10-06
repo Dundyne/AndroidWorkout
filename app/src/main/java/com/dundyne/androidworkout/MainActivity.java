102e4ad4a44b2cf0270d9cc2f25ee0fd58d2f1ae
Dundyne <55193759+Dundyne@users.noreply.github.com>
2020-10-06 15:14:27 +0200

    Firstestest Comet

--- app/src/main/java/com/dundyne/androidworkout/MainActivity.java
+++ app/src/main/java/com/dundyne/androidworkout/MainActivity.java
@@ -1,56 +1,56 @@
 package com.dundyne.androidworkout;
 
-import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
+import android.widget.Button;
+import android.widget.MediaController;
+import android.widget.TextView;
+import android.widget.VideoView;
 
-import android.view.Menu;
-import android.view.MenuItem;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
+    public int counter;
+    Button button;
+    TextView textView;
+    VideoView vid;
+    MediaController m;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        vid = (VideoView)findViewById(R.id.VideoViewSteroidz);
+        button= (Button) findViewById(R.id.BtnTimer);
+        textView= (TextView) findViewById(R.id.TimerCountdown);
+        button.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                counter = 60;
+                new CountDownTimer(30000, 1000){
+                    public void onTick(long millisUntilFinished){
+                        textView.setText(String.valueOf(counter));
+                        counter--;
+                    }
+                    public  void onFinish(){
+                        textView.setText("FINISH!!");
+                    }
+                }.start();
             }
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    public void playVideo(View v){
+        m = new MediaController(this);
+        /*String path = "C:/Users/Henri/Documents/GitHub/AndroidWorkout/funiVideo/catterrrs.mp4";*/
+        String path = "android.resource://com.dundyne.androidworkout/"+R.raw.cats;
+        Uri u = Uri.parse(path);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        vid.setVideoURI(u);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        vid.start();
     }
 }
