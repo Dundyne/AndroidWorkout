ee5cfca8e099c2564ead48612218d17aa35bded4
Robert <55789494+rabjaert@users.noreply.github.com>
2020-10-07 00:30:46 +0200

    Fikset timer

--- app/src/main/java/com/dundyne/androidworkout/MainActivity.java
+++ app/src/main/java/com/dundyne/androidworkout/MainActivity.java
@@ -5,11 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.MediaController;
 import android.widget.TextView;
 import android.widget.VideoView;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity  {
     public int counter;
@@ -18,32 +21,104 @@ public class MainActivity extends AppCompatActivity  {
     VideoView vid;
     MediaController m;
 
+    //** Stopwatch variables **//
+    private int seconds = 0;
+    private boolean running;
+    private boolean wasRunning;
+
+    //**              **//
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         vid = (VideoView)findViewById(R.id.VideoViewSteroidz);
         button= (Button) findViewById(R.id.BtnTimer);
-        textView= (TextView) findViewById(R.id.TimerCountdown);
-        button.setOnClickListener(new View.OnClickListener()
-        {
+        if (savedInstanceState != null) {
+            
+            seconds
+                    = savedInstanceState
+                    .getInt("seconds");
+            running
+                    = savedInstanceState
+                    .getBoolean("running");
+            wasRunning
+                    = savedInstanceState
+                    .getBoolean("wasRunning");
+        }
+        runTimer();
+    }
+
+
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState
+                .putInt("seconds", seconds);
+        savedInstanceState
+                .putBoolean("running", running);
+        savedInstanceState
+                .putBoolean("wasRunning", wasRunning);
+    }
+
+    protected void onPause()
+    {
+        super.onPause();
+        wasRunning = running;
+        running = false;
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        if (wasRunning) {
+            running = true;
+        }
+    }
+
+    public void startTimer(View view){running = true;}
+    public void stopTimer(View view){ running = false; }
+    public void resetTimer(View view){
+        running = false;seconds = 0;
+        vid.suspend();
+    }
+
+    private void runTimer() {
+
+        final TextView timeView
+                = (TextView) findViewById(
+                R.id.TimerCountdown);
+
+
+        final Handler handler
+                = new Handler();
+        handler.post(new Runnable() {
+
             @Override
-            public void onClick(View v) {
-                counter = 60;
-                new CountDownTimer(30000, 1000){
-                    public void onTick(long millisUntilFinished){
-                        textView.setText(String.valueOf(counter));
-                        counter--;
-                    }
-                    public  void onFinish(){
-                        textView.setText("FINISH!!");
-                    }
-                }.start();
+            public void run() {
+                int hours = seconds / 3600;
+                int minutes = (seconds % 3600) / 60;
+                int secs = seconds % 60;
+
+                String time
+                        = String
+                        .format(Locale.getDefault(),
+                                "%d:%02d:%02d", hours,
+                                minutes, secs);
+
+                timeView.setText(time);
+
+
+                if (running) {
+                    seconds++;
+                }
+
+                handler.postDelayed(this, 1000);
             }
         });
     }
 
-    public void playVideo(View v){
+            public void playVideo(View v){
         m = new MediaController(this);
         /*String path = "C:/Users/Henri/Documents/GitHub/AndroidWorkout/funiVideo/catterrrs.mp4";*/
         String path = "android.resource://com.dundyne.androidworkout/"+R.raw.cats;
