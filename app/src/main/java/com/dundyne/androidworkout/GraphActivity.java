d6b92db5453cae09fb79f6485d05b42ce0ac6280
Dundyne <55193759+Dundyne@users.noreply.github.com>
2020-11-23 13:09:43 +0100

    Bruh

--- app/src/main/java/com/dundyne/androidworkout/GraphActivity.java
+++ app/src/main/java/com/dundyne/androidworkout/GraphActivity.java
@@ -7,22 +7,34 @@ import android.os.Bundle;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
+import java.util.ArrayList;
 
 public class GraphActivity extends AppCompatActivity {
     GraphView linegraph;
+    TinyDB tinydb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graph);
-
+        tinydb = new TinyDB(getApplicationContext());
         linegraph = (GraphView) findViewById(R.id.line_graph);
-        LineGraphSeries<DataPoint> lineSeries = new LineGraphSeries<>(new DataPoint[] {
-                new DataPoint(0, 1),
-                new DataPoint(1, 5),
-                new DataPoint(2, 3),
-                new DataPoint(3, 2),
-                new DataPoint(4, 6)
-        });
-        linegraph.addSeries(lineSeries);
+
+       createGraph();
+
     }
+
+    public void createGraph(){
+
+        int count = 0;
+        ArrayList<Integer> progress = tinydb.getListInt("pushup");
+        DataPoint[] dataPoints = new DataPoint[progress.size()];
+
+        for(int val : progress){
+            dataPoints[count] = new DataPoint(count, val);
+            count++;
+        }
+        LineGraphSeries<DataPoint> lineSeriesTest = new LineGraphSeries<>(dataPoints);
+        linegraph.addSeries(lineSeriesTest);
+    }
+
 }
--- app/src/main/java/com/dundyne/androidworkout/MainActivity.java
+++ app/src/main/java/com/dundyne/androidworkout/MainActivity.java
@@ -24,114 +24,12 @@ public class MainActivity extends AppCompatActivity  {
     public static final String EXTRA_MESSAGE =
             "com.dundyne.androidworkout.extra.MESSAGE";
 
-    //** Stopwatch variables **//
-    private int seconds = 0;
-    private boolean running;
-    private boolean wasRunning;
-
-    //**              **//
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        vid = (VideoView)findViewById(R.id.VideoViewSteroidz);
-        button= (Button) findViewById(R.id.BtnTimer);
-        if (savedInstanceState != null) {
-            
-            seconds
-                    = savedInstanceState
-                    .getInt("seconds");
-            running
-                    = savedInstanceState
-                    .getBoolean("running");
-            wasRunning
-                    = savedInstanceState
-                    .getBoolean("wasRunning");
-        }
-        runTimer();
     }
-
-
-    public void onSaveInstanceState(Bundle savedInstanceState) {
-        super.onSaveInstanceState(savedInstanceState);
-        savedInstanceState
-                .putInt("seconds", seconds);
-        savedInstanceState
-                .putBoolean("running", running);
-        savedInstanceState
-                .putBoolean("wasRunning", wasRunning);
-    }
-
-    protected void onPause()
-    {
-        super.onPause();
-        wasRunning = running;
-        running = false;
-    }
-
-    @Override
-    protected void onResume()
-    {
-        super.onResume();
-        if (wasRunning) {
-            running = true;
-        }
-    }
-
-    public void startTimer(View view){running = true;}
-    public void stopTimer(View view){ running = false; }
-    public void resetTimer(View view){
-        running = false;seconds = 0;
-        vid.suspend();
-    }
-
-    private void runTimer() {
-
-        final TextView timeView
-                = (TextView) findViewById(
-                R.id.TimerCountdown);
-
-
-        final Handler handler
-                = new Handler();
-        handler.post(new Runnable() {
-
-            @Override
-            public void run() {
-                int hours = seconds / 3600;
-                int minutes = (seconds % 3600) / 60;
-                int secs = seconds % 60;
-
-                String time
-                        = String
-                        .format(Locale.getDefault(),
-                                "%d:%02d:%02d", hours,
-                                minutes, secs);
-
-                timeView.setText(time);
-
-
-                if (running) {
-                    seconds++;
-                }
-
-                handler.postDelayed(this, 1000);
-            }
-        });
-    }
-
-            public void playVideo(View v){
-        m = new MediaController(this);
-        /*String path = "C:/Users/Henri/Documents/GitHub/AndroidWorkout/funiVideo/catterrrs.mp4";*/
-        String path = "android.resource://com.dundyne.androidworkout/"+R.raw.cats;
-        Uri u = Uri.parse(path);
-
-        vid.setVideoURI(u);
-
-        vid.start();
-    }
-
         public void launchSecondActivity(View v){
         Intent intent = new Intent(this, SecondActivity.class);
             intent.putExtra(EXTRA_MESSAGE, "test");
@@ -139,7 +37,17 @@ public class MainActivity extends AppCompatActivity  {
     }
 
         public void loginActivity(View v){
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    public void exerciseActivity(View v){
         Intent intent = new Intent(this, ExerciseActivity.class);
+        startActivity(intent);
+    }
+
+    public void settingsActivity(View v){
+        Intent intent = new Intent(this, SettingsActivity.class);
         startActivity(intent);
     }
 
