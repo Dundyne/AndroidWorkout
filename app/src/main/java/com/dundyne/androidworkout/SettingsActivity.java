67b86746bed69b714f8f3c0eaeb2b5cc53ea7889
Robert <55789494+rabjaert@users.noreply.github.com>
2020-11-23 22:35:32 +0100

    Ordnet dark mode i settings. Blir dark mode på alle viewsa så det er ganske krem as

--- app/src/main/java/com/dundyne/androidworkout/SettingsActivity.java
+++ app/src/main/java/com/dundyne/androidworkout/SettingsActivity.java
@@ -1,16 +1,30 @@
 package com.dundyne.androidworkout;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.preference.CheckBoxPreference;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceGroup;
 
 public class SettingsActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+
+
+
+
         setContentView(R.layout.settings_activity);
         getSupportFragmentManager()
                 .beginTransaction()
@@ -22,10 +36,31 @@ public class SettingsActivity extends AppCompatActivity {
         }
     }
 
+
+
     public static class SettingsFragment extends PreferenceFragmentCompat {
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey);
+
+            final CheckBoxPreference checkboxPref = (CheckBoxPreference) getPreferenceManager().findPreference("checkbox_pref");
+
+            checkboxPref.
+                    setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+
+                        public boolean onPreferenceChange(Preference preference, Object newValue) {
+                            if (newValue.toString().equals("true")) {
+                                Log.d("myTag", "dette den er huket på true!");
+                               AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                            } else {
+                              Log.d("FalseTag", "den er huket på false");
+                                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                            }
+                            return true;
+                        }
+                    });
+
         }
     }
 }
+
